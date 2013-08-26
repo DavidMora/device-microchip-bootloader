b19502bb429e7e64a589ead25f7a3acad0801884
Lieven Hollevoet <hollie@lika.be>
2013-08-26 22:06:24 +0200

    Build results of 537eed7 (on master)

--- lib/Device/Microchip/Bootloader.pm
+++ lib/Device/Microchip/Bootloader.pm
@@ -2,7 +2,7 @@ use strict;  # To keep Test::Perl::Critic happy, Moose does enable this too...
 
 package Device::Microchip::Bootloader;
 {
-  $Device::Microchip::Bootloader::VERSION = '0.3';
+  $Device::Microchip::Bootloader::VERSION = '0.4';
 }
 
 use Moose;
@@ -821,7 +821,7 @@ Device::Microchip::Bootloader - Bootloader host software for Microchip PIC devic
 
 =head1 VERSION
 
-version 0.3
+version 0.4
 
 =head1 SYNOPSIS
 
